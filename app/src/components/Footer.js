34b3588f71364d5da9c22fd9bf65962c341cb285
preigile <diva.artim@gmail.com>
2018-08-10 22:25:35 +0300

    create footer component

---
+++ app/src/components/Footer.js
@@ -0,0 +1,10 @@
+export default () => {
+    return `<div class="footer">
+                <nav class="footer-nav">
+                    <a class="footer-nav-item" href="#" target="_self">Помощь</a>
+                    <a class="footer-nav-item" href="#" target="_self">Обратная связь</a>
+                    <a class="footer-nav-item" href="#" target="_self">Разработчикам</a>
+                    <a class="footer-nav-item" href="#" target="_self">Условия использовуания</a>
+                </nav>
+            </div>`
+}
