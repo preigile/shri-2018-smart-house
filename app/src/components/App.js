0e012763917901b0be3aff38a31389d3ffccbfa3
preigile <diva.artim@gmail.com>
2018-08-10 21:06:10 +0300

    create header component

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,5 +1,13 @@
 import Header from "./Header";
+import Main from "./Main";
+import Scenarios from "./Scenarios";
+import Devices from "./Devices";
+import Footer from "./Footer";
 
 export default () => {
-    return `${Header()}`;
+    return `${Header()}
+            ${Main()}
+            ${Scenarios()}
+            ${Devices()}
+            ${Footer()}`;
 }
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,5 +1,10 @@
 export default () => {
     return `<div class="header">
-                <h3 class="logo">Smart House</h3>
+                <h3 class="header-logo"></h3>
+                <nav class="header-nav">
+                    <a class="header-nav-item" href="#" target="_self">Сводка</a>
+                    <a class="header-nav-item" href="#" target="_self">Устройства</a>
+                    <a class="header-nav-item" href="#" target="_self">Сценарии</a>
+                </nav>
             </div>`
 }
