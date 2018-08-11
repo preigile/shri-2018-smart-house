e5531745fc37eb3389ac8fb2563332aca54b0b7d
preigile <diva.artim@gmail.com>
2018-08-11 22:15:42 +0300

    create major component

---
+++ app/src/components/Major.js
@@ -0,0 +1,22 @@
+export default () => {
+    return `<div class="major">
+                <div class="major-title">Главное</div>
+                <div class="major-cards-container">
+                    <div class="major-cards-info">
+                        <div class="major-cards-info-title">Привет, Геннадий!</div>
+                        <div class="major-cards-info-description">Двери и окна закрыты, сигнализация включена.</div>
+                        <div class="major-cards-info-weather">
+                            <div class="weather-home">
+                                <div class="weather-title">Дома</div>
+                                <div class="weather-number">+23</div>
+                            </div>
+                            <div class="weather-street">
+                                <div class="weather-title">За окном</div>
+                                <div class="weather-number">+19</div>
+                            </div>
+                            <div class="weather-icon"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
