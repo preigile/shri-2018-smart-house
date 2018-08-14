652e793e1525d68143c8325929aad6ae54509a09
preigile <diva.artim@gmail.com>
2018-08-14 22:30:53 +0300

    header to BEM

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,10 +1,10 @@
 export default () => {
     return `<div class="header">
-                <h3 class="header-logo"></h3>
-                <nav class="header-nav">
-                    <a class="header-nav-item" href="#" target="_self">Сводка</a>
-                    <a class="header-nav-item" href="#" target="_self">Устройства</a>
-                    <a class="header-nav-item" href="#" target="_self">Сценарии</a>
+                <h3 class="header__logo"></h3>
+                <nav class="header__nav">
+                    <a class="header__nav-item header__nav-item_selected" href="#" target="_self">Сводка</a>
+                    <a class="header__nav-item" href="#" target="_self">Устройства</a>
+                    <a class="header__nav-item" href="#" target="_self">Сценарии</a>
                 </nav>
             </div>`
 }
