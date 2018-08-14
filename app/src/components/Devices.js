7ccfa5ecee02f3eab4b30ed173349b52529ee2a0
preigile <diva.artim@gmail.com>
2018-08-14 21:38:37 +0300

    fill on devices

---
+++ app/src/components/Devices.js
@@ -0,0 +1,21 @@
+import Card from "./Card";
+import devicesInfo from "../starage/devices";
+import devicesTypes from "../starage/devicesTypes";
+
+export default () => {
+    return `<div class="devices">
+                <div class="devices-types">
+                    <div class="devices-types-title title">Избранные устройства</div>
+                    ${devicesTypes().map(devicesType).join('')}
+                </div>
+                <div class="devices-container">
+                    ${devicesInfo().map(Card).join('')}
+                </div>
+            </div>`
+}
+
+function devicesType (type) {
+    return `<div class="devices-type">
+                <div class="devices-type-title">${type.title}</div>
+            </div>`
+}
---
+++ app/src/starage/devices.js
@@ -0,0 +1,62 @@
+let devices = [
+  {
+    id: 1,
+    title: 'Xiaomi Yeelight LED Smart Bulb',
+    description: 'Включено',
+    icon: 'icon_sun_yellow.svg',
+    type: ''
+  },
+  {
+    id: 2,
+    title: 'D-Link Omna 180 Cam',
+    description: 'Включится в 17:00',
+    icon: 'icon_sun.svg',
+    type: ''
+  },
+  {
+    id: 3,
+    title: 'Elgato Eve Degree Connected',
+    description: 'Выключено до 17:00',
+    icon: 'icon_temperature.svg',
+    type: ''
+  },
+  {
+    id: 4,
+    title: 'LIFX Mini Day & Dusk A60 E27',
+    description: 'Включится в 17:00',
+    icon: 'icon_sun.svg',
+    type: ''
+  },
+  {
+    id: 5,
+    title: 'Xiaomi Mi Air Purifier 2S',
+    description: 'Включено',
+    icon: 'icon_sun_yellow.svg',
+    type: ''
+  },
+  {
+    id: 6,
+    title: 'Philips Zhirui',
+    description: 'Выключено',
+    icon: 'icon_sun.svg',
+    type: ''
+  },
+  {
+    id: 7,
+    title: 'Xiaomi Mi Air Purifier 2S',
+    description: 'Включено',
+    icon: 'icon_sun_yellow.svg',
+    type: ''
+  },
+  {
+    id: 8,
+    title: 'Philips Cooler',
+    description: 'Начнет охлаждать в 16:30',
+    icon: 'icon_sun.svg',
+    type: ''
+  }
+];
+
+export default () => {
+  return devices;
+}
